3d96228cdcf7b3cbded28335f69138e5239bca27
Brilliahib <brilliahib21@gmail.com>
2023-10-09 00:14:35 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("scroll", function () {
+    var navbar = document.querySelector(".myNavbar");
+    if (window.scrollY > 0) {
+        navbar.style.boxShadow = "0 1px 2px rgba(0, 0, 0, 0.1)";
+    } else {
+        navbar.style.boxShadow = "none";
+    }
+});
