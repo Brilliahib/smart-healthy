f1ed8ee0a0db4290372db13dcc574bd33582f6d3
Brilliahib <brilliahib21@gmail.com>
2023-10-09 20:59:00 +0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", function () {
     var navbar = document.querySelector(".myNavbar");
     if (window.scrollY > 0) {
-        navbar.style.boxShadow = "0 1px 2px rgba(0, 0, 0, 0.1)";
+        navbar.style.boxShadow = "0 1px 3px rgba(0, 0, 0, 0.08)";
     } else {
         navbar.style.boxShadow = "none";
     }
